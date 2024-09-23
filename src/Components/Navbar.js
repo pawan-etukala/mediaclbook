d3d8a136544848838dbadfc915f8c8381711e361
Your Name <pawanetukala@gmail.com>
2024-09-23 16:34:14 +0530

    added medicalbook

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/Navbar.css';
 import logo from '../Assets/homeo.jpg';
-import { Link } from 'react-router-dom';git add .
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
